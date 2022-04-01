29ab8dfec9c4c15ed5d25848ed5f1dcb9b40b017
billyfrn <71398041+billyfrn@users.noreply.github.com>
2022-04-01 09:46:19 -0700

    Create portfolio.js

---
+++ src/pages/portfolio.js
@@ -0,0 +1,13 @@
+import * as React fromm 'react'
+
+const PortfolioPage = () => {
+ return(
+  <main>
+   <title>Portfolio page</title>
+   <h1>Portfolio page title</h1>
+   <p>hi this is the portfolio page</p>
+  </main>
+ ) 
+}
+
+export defauly PortfolioPage

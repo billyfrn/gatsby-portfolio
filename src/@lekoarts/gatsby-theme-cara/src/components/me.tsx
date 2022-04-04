1a419d20e9dec884e3ba416e99626e4b21744f05
billyfrn <71398041+billyfrn@users.noreply.github.com>
2022-04-04 14:23:07 -0700

    Update me.tsx

--- src/@lekoarts/gatsby-theme-cara/src/components/me.tsx
+++ src/@lekoarts/gatsby-theme-cara/src/components/me.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Helmet } from "react-helmet"
 import { withPrefix } from "gatsby"
 
-const Me = (props) => {
+const Me = () => {
   return (
     <img src={withPrefix(`/bf-pxl.png`)} alt="Billy, Front end Web dev" />
   );

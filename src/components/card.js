6e83040323f3ff41ddbf42a7093161ff6ac6d8a9
billyfrn <71398041+billyfrn@users.noreply.github.com>
2022-04-04 10:59:13 -0700

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,1 +1,17 @@
+import React, { Component } from "react";
 
+class Card extends Component {
+  // ...
+  render() {
+    return (
+      <main>
+        <h1>React Modal</h1>
+        <button type="button" onClick={this.showModal}>
+          Open
+        </button>
+      </main>
+    );
+  }
+}
+
+export default Card

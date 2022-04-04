2b4294847828752db6abe165f83f4874b70cf4de
billyfrn <71398041+billyfrn@users.noreply.github.com>
2022-04-04 14:34:11 -0700

    Update me.tsx

--- src/@lekoarts/gatsby-theme-cara/src/components/me.tsx
+++ src/@lekoarts/gatsby-theme-cara/src/components/me.tsx
@@ -3,10 +3,10 @@
 import * as React from "react"
 import { withPrefix } from "gatsby"
 
-const Me = () => {
+const PixelMe = () => {
   return (
     <img src={withPrefix(`/bf-pxl.png`)} alt="Billy, Front end Web dev" />
   );
 };
 
-export default Me
+export default PixelMe

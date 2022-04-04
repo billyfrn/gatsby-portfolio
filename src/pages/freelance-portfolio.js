ff0a4be69ae8a2c177a97f618a2587da33f0764c
billyfrn <71398041+billyfrn@users.noreply.github.com>
2022-04-04 14:43:28 -0700

    Create freelance-portfolio.js

---
+++ src/pages/freelance-portfolio.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import "./styles.css"
+
+const FreelancePortfolioPage = () => {
+ return(
+  <main>
+   <title>Agency Portfolio page</title>
+   <h1>Agency work</h1>
+   <p>hi this is the agency portfolio page</p>
+  </main>
+ ) 
+}
+
+export default FreelancePortfolioPage

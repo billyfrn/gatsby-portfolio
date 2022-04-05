384d62ee6c948348868532aaa790d087a8eef616
billyfrn <71398041+billyfrn@users.noreply.github.com>
2022-04-05 12:08:10 -0700

    Update cara.tsx

--- src/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx
@@ -5,6 +5,7 @@ import Hero from "../components/hero"
 import Projects from "../components/projects"
 import About from "../components/about"
 import Contact from "../components/contact"
+import "./styles.css"
 
 const Cara = () => (
   <Layout>

e09da7fe0a7c729fec4c848b486ff711970f9b3e
billyfrn <71398041+billyfrn@users.noreply.github.com>
2022-04-05 16:24:06 -0700

    Update agency-portfolio.js

--- src/pages/agency-portfolio.js
+++ src/pages/agency-portfolio.js
@@ -3,16 +3,20 @@ import SEO from "../@lekoarts/gatsby-theme-cara/src/components/seo"
 import "./styles.css"
 
 const PortfolioPage = () => {
- <React.Fragment>
-  <SEO />
-  <main>
-   <title>Agency Portfolio page</title>
-   <div class='wrapper'>
-    <h1>Agency work</h1>
-    <p>hi this is the agency portfolio page</p>
-   </div>
-  </main>
- </React.Fragment>
+ render() {
+    return (
+     <React.Fragment>
+      <SEO />
+      <main>
+       <title>Agency Portfolio page</title>
+       <div class='wrapper'>
+        <h1>Agency work</h1>
+        <p>hi this is the agency portfolio page</p>
+       </div>
+      </main>
+     </React.Fragment>
+     )
+ }
 }
 
 export default PortfolioPage
